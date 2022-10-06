c0f8e6738c590e4f9ba93f65ac59beffe57a1764
Dario Binacchi <dario.binacchi@amarulasolutions.com>
2022-10-06 18:08:36 +0000

    simplify build.sh updating on new toolchain creation
    The patch aims to obtain a build.sh script in which the changes to be
    made are minimal if the toolchain needs to be updated.
    
    Jira-Id: WAN-831
    Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@
 # FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-set -e
+set -ex
 
 if [ "$#" -ne 1 ]; then
 	echo "Usage:" >&2
@@ -25,6 +25,10 @@ if [ "$#" -ne 1 ]; then
 	echo "  $0 x86-i686" >&2
 	exit 1
 fi
+
+tc_tag="2021.05"
+tc_dir="$1--uclibc--stable-${tc_tag}"
+tc_tar="$1--uclibc-backtrace-cortex_a7--stable-${tc_tag}.tar.bz2"
 
 if [[ "$1" != armv5-eabi && "$1" != armv7-eabihf && "$1" != x86-i686 ]]; then
 	echo "Unsupported platform" >&2
@@ -35,13 +39,13 @@ if [ ! -d "./buildroot" ]; then
 	git clone https://github.com/bootlin/buildroot-toolchains.git buildroot
 fi
 
-if [ -f "$1"--uclibc-backtrace-cortex_a7--stable-2021.05.tar.bz2 ]; then
-	rm "$1"--uclibc-backtrace-cortex_a7--stable-2021.05.tar.bz2
+if [ -f "${tc_tar}" ]; then
+	rm "${tc_tar}"
 fi
 
 cd buildroot
 git clean -fdx
-git checkout toolchains.bootlin.com-2021.05
+git checkout toolchains.bootlin.com-"${tc_tag}"
 
 cat ../"$1"-uclibc-config > ./.config
 cp ../uclibc-backtrace.fragment ./
@@ -51,5 +55,5 @@ make clean
 make
 make sdk
 
-cp ../"$1"-readme.txt ./"$1"--uclibc--stable-2021.05
-tar -cjf ../"$1"--uclibc-backtrace-cortex_a7--stable-2021.05.tar.bz2 "$1"--uclibc--stable-2021.05
+cp ../"$1"-readme.txt ./"${tc_dir}"
+tar -cjf ../"${tc_tar}" "${tc_dir}"
